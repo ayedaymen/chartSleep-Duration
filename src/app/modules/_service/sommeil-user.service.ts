21f15144ce7bffff7c91ef99183bd8c200f65eba
Aymen Ayed <aymen.ayed@esprit.tn>
2021-04-19 10:12:28 +0200

    sommeil-userservice

--- src/app/modules/_service/sommeil-user.service.ts
+++ src/app/modules/_service/sommeil-user.service.ts
@@ -12,6 +12,6 @@ export class SommeilUserService {
   
   getAllData(): Observable<Someil[]> {
   
-    return this.http.get<Someil[]>('http://prod.kaisens.fr:811/api/sleep/?deviceid=93debd97-6564-454b-be33-35bd377a2563&startdate=1612310400000&enddate=1614729600000');
+    return this.http.get<Someil[]>('https://cors-anywhere.herokuapp.com/http://prod.kaisens.fr:811/api/sleep/?deviceid=93debd97-6564-454b-be33-35bd377a2563&startdate=1612310400000&enddate=1614729600000');
   }
 }
