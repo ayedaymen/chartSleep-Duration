214bcdabe172c8089d89f3b894cb97a14023c26b
Aymen Ayed <aymen.ayed@esprit.tn>
2021-04-20 19:10:31 +0200

    cross resourve probleme

--- src/app/modules/_service/sommeil-user.service.ts
+++ src/app/modules/_service/sommeil-user.service.ts
@@ -11,7 +11,7 @@ export class SommeilUserService {
   constructor(private http: HttpClient) { }
   
   getAllData(): Observable<Someil[]> {
-  
-    return this.http.get<Someil[]>('https://cors-anywhere.herokuapp.com/http://prod.kaisens.fr:811/api/sleep/?deviceid=93debd97-6564-454b-be33-35bd377a2563&startdate=1612310400000&enddate=1614729600000');
+
+    return this.http.get<Someil[]>(' https://morning-savannah-56718.herokuapp.com/http://prod.kaisens.fr:811/api/sleep/?deviceid=93debd97-6564-454b-be33-35bd377a2563&startdate=1612310400000&enddate=1614729600000');
   }
 }
